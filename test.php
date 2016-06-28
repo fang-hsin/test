cd936ac836655ca383e3395ed2751d0707b346ef
fang-hsin <play0926365690@yahoo.com.tw>
2016-06-28 14:56:42 +0800

    第一版

---
+++ test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>使用者登入</title>
+</head>
+<body>
+  <p></p>
+  <hr>
+  <form action="" method="post">
+      帳號：<input type="text" name="usrName" 
+	               value="LKK" required><br>
+      密碼：<input type="password" name="usrPass" 
+	               value="lkk" required>
+    <input type="submit" value="送出">
+  </form>
+</body>
+</html>
